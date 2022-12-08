1e722af918eae15270b69f95eb92dd8f563eb850
noobnhut <nguyen.nhut.99.2017@gmail.com>
2022-12-08 21:40:48 +0700

    lab2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //sidebar
 const menuItems = document.querySelectorAll('.menu-item');
-const messagesNotification = document.querySelector('#messages-notifications');
+
 const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
@@ -13,19 +13,6 @@ const changeActiveItem = () => {
 }
  
 
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        changeActiveItem();
-        item.classList.add('active');
-        
-        if (item.id != 'notifications') {
-            document.querySelector('.notifications-popup').style.display = 'none';
-        } else {
-            document.querySelector('.notifications-popup').style.display = 'block';
-            document.querySelector('#notifications .notification-count').style.display='none';
-        }
-    })
-})
 
 
 const searchMessage = () => {
