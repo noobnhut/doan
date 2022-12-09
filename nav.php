7ea7113524b14fbfa8eae39170aa23d5159d98ef
noobnhut <nguyen.nhut.99.2017@gmail.com>
2022-12-09 15:39:51 +0700

    lab88

---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav>
+            <div class="container">
+                <h2 class="logo">404NotFound</h2>
+                <div class="search-bar">
+                    <i class="uil uil-search"></i>
+                    <input
+                    type="search"
+                    placeholder="Tìm kiếm gì đó nào ......."
+                    />
+                </div>
+                <div class="create">
+                    <label class="btn btn-primary" for="">Đăng bài</label>
+                    <div class="profile-pic">
+                        <img src="images/profile-8.jpg" alt="pic 1" />
+                    </div>
+                </div>
+            </div>
+      </nav>
